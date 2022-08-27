a74b1f8cb330599e61bd806b48481eda22c69146
Andieferson Machado <102043875+Andieferson@users.noreply.github.com>
2022-08-27 09:14:22 -0300

    vetor e função

---
+++ desafios/analisador/script.js
@@ -0,0 +1,7 @@
+function adicionar() {
+    var num = document.getElementById('txtn')
+    var caixa = document.getElementById('icaixa')
+    if (num.value.length == 0) {
+        window.alert('Digite um valor no local especifico')
+    }
+}
